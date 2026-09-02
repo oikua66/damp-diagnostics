46b937ba9ed6780011782813f5fdf1959c4a1c22
oikua66 <oikua66@gmail.com>
2026-09-02 06:55:43 +0200

    Add September damp contact form to homepage contact section

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,6 +14,7 @@ import LegalFooter from './components/LegalFooter';
 import HeaderAboutInjector from './components/HeaderAboutInjector';
 import MobileMenu from './components/MobileMenu';
 import SeptemberDampPromo from './components/SeptemberDampPromo';
+import SeptemberDampLeadInjector from './components/SeptemberDampLeadInjector';
 
 export const metadata: Metadata = {
   title: 'Koretskiy Consulting',
@@ -31,6 +32,7 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
         <HeaderAboutInjector />
         <MobileMenu />
         {children}
+        <SeptemberDampLeadInjector />
         <ContactDetails />
         <LegalFooter />
         <Analytics />
