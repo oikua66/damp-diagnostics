5bbd3ef174f1751df5e6a31665c3e81354b00b5a
oikua66 <oikua66@gmail.com>
2026-09-02 12:29:25 +0200

    Add localized subscription shortcut routes

---
+++ app/[lang]/subscribe/page.tsx
@@ -0,0 +1,10 @@
+import { redirect } from 'next/navigation';
+import { languages, type Lang } from '../../../../lib/translations';
+
+type Props = { params: Promise<{ lang: string }> };
+
+export default async function SubscribeRedirect({ params }: Props) {
+  const { lang: rawLang } = await params;
+  const lang = languages.includes(rawLang as Lang) ? rawLang : 'en';
+  redirect(`/${lang}#subscribe`);
+}
